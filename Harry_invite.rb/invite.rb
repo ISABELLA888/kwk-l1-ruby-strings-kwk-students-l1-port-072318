1d7854a0e508f77ebd9fb790af6cf19f6d45bbda
ISABELLA888 <ipcastro888@gmail.com>
2018-07-23 22:31:22 +0000

    automatically backed up by learn

--- Harry_invite.rb/invite.rb
+++ Harry_invite.rb/invite.rb
@@ -10,7 +10,9 @@ puts "What time?"
 time = gets.strip
 puts "rvsp day?"
 date2 = gets.strip
+puts "rvsp month?"
 month2 = gets.strip
+puts "your name?"
 hname = gets.strip
 puts "Dear #{guest}"
 puts "You are cordially invited to #{name} on #{month} + #{day} at #{time}. Please rsvp no later than #{month2} + #{day2}. "  
