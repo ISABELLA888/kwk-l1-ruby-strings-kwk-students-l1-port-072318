b5f88aa03e837476cef854b8c0f281337fd317c9
ISABELLA888 <ipcastro888@gmail.com>
2018-07-23 22:30:34 +0000

    automatically backed up by learn

--- Harry_invite.rb/invite.rb
+++ Harry_invite.rb/invite.rb
@@ -1,9 +1,14 @@
 puts "Please enter your guest's name!"
 guest = gets.strip 
+puts "What is the party name?"
 name = gets.strip
+puts "What month?"
 month = gets.strip
+puts "Day of the month?"
 day = gets.strip.to_i
+puts "What time?"
 time = gets.strip
+puts "rvsp day?"
 date2 = gets.strip
 month2 = gets.strip
 hname = gets.strip
