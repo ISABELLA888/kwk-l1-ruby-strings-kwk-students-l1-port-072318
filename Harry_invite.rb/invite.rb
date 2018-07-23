d42661c53fa4147cd27da3f3125023ce46e9685e
ISABELLA888 <ipcastro888@gmail.com>
2018-07-23 22:19:18 +0000

    automatically backed up by learn

--- Harry_invite.rb/invite.rb
+++ Harry_invite.rb/invite.rb
@@ -1,7 +1,6 @@
 guest = gets.strip 
-puts "Dear #{guest}" 
 name = the_party
 date = july 23
 time = neveroclock
-
+puts "Dear #{guest}"
 puts "You are cordially invited to #{name} on #{date} at #{time} "  
