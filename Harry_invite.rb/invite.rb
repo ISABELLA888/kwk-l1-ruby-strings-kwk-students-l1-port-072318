c7dde8bb1b0d68460d1949a3e254ede8193850f2
ISABELLA888 <ipcastro888@gmail.com>
2018-07-23 22:16:14 +0000

    automatically backed up by learn

--- Harry_invite.rb/invite.rb
+++ Harry_invite.rb/invite.rb
@@ -1,2 +1,7 @@
+guest = gets.strip 
 puts "Dear #{guest}" 
-guest = gets.strip. 
+name = the party
+date = july 23
+time = never 'o' clock 
+
+puts "You are cordially invited to #{name} on #{date} at #{time}   
