3ff45d57c1cfebcb2cfeb7dc7162f1f55ec7cbde
ISABELLA888 <ipcastro888@gmail.com>
2018-07-23 22:16:43 +0000

    automatically backed up by learn

--- Harry_invite.rb/invite.rb
+++ Harry_invite.rb/invite.rb
@@ -4,4 +4,4 @@ name = the party
 date = july 23
 time = never 'o' clock 
 
-puts "You are cordially invited to #{name} on #{date} at #{time}   
+puts "You are cordially invited to #{name} on #{date} at #{time} "  
